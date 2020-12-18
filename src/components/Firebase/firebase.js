d8748028dc7cd4e66edfb2575e324bf8cb8bcbcb
MishaAkram <mishaakrambaig@gmail.com>
2020-12-18 14:28:44 +0500

    hosting url changed

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,16 +2,15 @@ import app from 'firebase/app';
 import 'firebase/auth';
 
 import 'firebase/database';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const config = {
-
-  apiKey: "AIzaSyC5hVlr672LqHE6wAG34Sa5D0XxyC-Cxow",
-  authDomain: "auth-fec89.firebaseapp.com",
-  databaseURL: "https://auth-fec89-default-rtdb.firebaseio.com",
-  projectId: "auth-fec89",
-  storageBucket: "auth-fec89.appspot.com",
-  messagingSenderId: "96933822347",
-  appId: "1:96933822347:web:67850af0edf0f18d1b398e",
-  measurementId: "G-0RM1FX9QQY"
+  apiKey: "AIzaSyCRYfIRnsoOBccOJC8RV_KrTERVoJV1CRM",
+  authDomain: "fir-login-496e8.firebaseapp.com",
+  projectId: "fir-login-496e8",
+  storageBucket: "fir-login-496e8.appspot.com",
+  messagingSenderId: "658349646822",
+  appId: "1:658349646822:web:761d7b65c85bce84c6ec27",
+  measurementId: "G-Y3JHMDZYLT"
 };
 
 class Firebase {
